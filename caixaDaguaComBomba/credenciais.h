e002b24bb704ea39909fea039f78d8ff8f5e9a14
ronaldopborges <ronaldo.borges@alunos.ufersa.edu.br>
2024-11-10 16:40:28 -0300

    beta apresentável

--- caixaDaguaComBomba/credenciais.h
+++
@@ -1,5 +0,0 @@
-const char* wifi_name = "iot_net";
-const char* wifi_pass = "ufersaPPGCC#2024";
-const char* mqtt_server = "192.168.0.3";
-const char* mqtt_user = "raspi";
-const char* mqtt_pass = "iotnet";
